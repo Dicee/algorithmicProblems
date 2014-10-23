25c84503bd31510fa9ae4202e37e86de4413b65d
David <Dicee>
2014-10-23 21:55:28 +0200

    Added a respectable problem

---
+++ codility/lesson2/MissingIntegerJava.java
@@ -0,0 +1,36 @@
+package lesson2;
+
+import java.util.*;
+import java.util.stream.*;
+
+class MissingIntegerJava {
+	class Pair {
+        public final int key;
+        public final boolean value;
+
+        public Pair(int key, boolean value) {
+            this.key   = key;
+            this.value = value;
+        }
+    }
+
+    public int solution(int[] A) {
+        int N = A.length;
+
+        boolean[] tab = new boolean[N];
+
+
+        for (int i=0; i < N; i++) {
+            if (A[i] <= N && A[i] > 0)
+                tab[A[i]-1] = true;
+        }
+
+        // IntStream.rang upper bound is exclusive so we need to use N+1
+        Optional<Pair> option = IntStream.range(1,N + 1)
+            .mapToObj(i -> new Pair(i,tab[i - 1]))
+            .filter(x -> !x.value)
+            .findFirst();
+
+        return option.isPresent() ? option.get().key : N + 1;
+    }
+}
