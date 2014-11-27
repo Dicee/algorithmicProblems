e23566625e8385d7e6c9ca0b7e23d91fe4bc45ae
Dicee <Dicee@users.noreply.github.com>
2014-11-27 16:56:18 +0100

    Update PermMissingElements.java

--- codility/lesson1/PermMissingElements.java
+++ codility/lesson1/PermMissingElements.java
@@ -1,15 +1,13 @@
 /**
  * Level : painless
  */
-class PermMissingElements {
-    public int solution(int[] A) {
-        long sum         = 0; 
-		long completeSum = 0;
-        for (int i=0 ; i<A.length ; i++) {
-            sum         += A[i];
-			completeSum += i + 1;
-		}
-        return (int) (completeSum - sum);
-    }
+ class PermMissingElements {
+	public int solution(int[] A) {
+        	long sum = 0;
+        	long n = A.length;
+        	for (int i=0 ; i<A.length ; sum += A[i++]);   
+        	return (int) ((n + 1)*(n + 2)/2 - sum);
+    	}
 }
+
 
