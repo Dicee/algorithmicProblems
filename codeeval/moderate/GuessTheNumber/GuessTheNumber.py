ca7e138556cd0fe757057f3cdf808d245b6e7e8e
Dicee <courtinot.david@orange.fr>
2015-01-05 13:40:34 +0100

    No comment

---
+++ codeeval/moderate/GuessTheNumber/GuessTheNumber.py
@@ -0,0 +1,16 @@
+﻿import sys
+lines = open(sys.argv[1], 'r')
+for line in lines:
+	split  = line.split(" ")
+	lower  = 0
+	higher = int(split[0])
+	mid    = (lower + higher + 1) // 2
+	for hint in split[1:]:
+		if hint == "Lower":
+			higher = mid - 1
+		elif hint == "Higher":
+			lower  = mid + 1
+		else:
+			print(mid)			
+		mid = (lower + higher + 1) // 2
+lines.close()
---
+++ codeeval/moderate/LongestLines/LongestLines.py
@@ -0,0 +1,50 @@
+﻿import sys
+from heapq import *
+
+class StringWrapper:
+	def __init__(self,s):
+		self.s = "".join([ x for x in s if x != "\n" ])
+	
+	def __eq__(self,that):
+		return len(self.s) == len(that.s)
+		
+	def __lt__(self,that):
+		return len(self.s) < len(that.s)
+		
+	def __gt__(self,that):
+		return len(self.s) > len(that.s)
+		
+	def __le__(self,that):
+		return len(self.s) <= len(that.s)
+		
+	def __ge__(self,that):
+		return len(self.s) >= len(that.s)
+		
+	def __str__(self):
+		return self.s
+		
+	def __repr__(self):
+		return self.s
+
+if __name__ == "__main__":
+	n     = 0
+	heap  = []
+	lines = open(sys.argv[1], 'r')
+	for line in lines:
+		if n == 0:
+			n = int(line)
+		else:
+			wrapper = StringWrapper(line)
+			if len(heap) == n:
+				if wrapper >= heap[0]:
+					heapreplace(heap,wrapper)
+			else:
+				heappush(heap,wrapper)
+			
+	res = [0]*len(heap)
+	i   = len(heap) - 1
+	while len(heap) > 0:
+		res[i] = heappop(heap).s
+		i     -= 1
+	print("\n".join(res))
+	lines.close()
