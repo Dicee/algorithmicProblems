4be05d6ccbb759b99bc9601fef39d7191575c7d8
Dicee <courtinot.david@orange.fr>
2015-01-12 13:44:43 +0100

    No comment

---
+++ codeeval/moderate/FindASquare/Main.scala
@@ -0,0 +1,57 @@
+﻿import scala.collection.mutable.ArrayBuffer
+import java.util.regex.Pattern
+import scala.util.matching.Regex
+
+// We will use a general algorithm working also with pointing float coordinates
+object Main extends App {
+    val regex = new Regex("\\((\\d+),(\\d)+\\)")
+    
+    scala.io.Source.fromFile(args(0)).getLines
+    	.filter(!_.isEmpty)
+        .map(line => {
+            val m   = regex.findAllIn(line)
+            val pts = m.map(x => Point(m.group(1).toFloat,m.group(2).toFloat)).toArray
+            
+            // we choose arbitrarily two lines with the 4 points. The lines may be
+            // parallel, in that case we try to swap the order and get orthogonal lines
+            var (v0,v1) = (pts(1) - pts(0),pts(3) - pts(2))
+            
+            v0 != Vector() && v1 != Vector() && 
+            	(if (v0 == v1 || v0 == -v1) {
+                    val (a,b) = if (v0 == v1) ((0,2),(1,3)) else ((0,3),(1,2))
+                    pts(a._1) - pts(a._2) == pts(b._1) - pts(b._2)
+                } else Point.cmp(v0.cross(v1)) && (v0.norm,v0.mid) == (v1.norm,v1.mid))
+        })
+        .foreach(println)
+}
+
+class Point(val x: Float = 0, val y: Float = 0) {
+	def unary_-           = Point (-x      ,-y      )
+    def +    (p: Point)   = Point ( x + p.x, y + p.y)
+    def -    (p: Point)   = Vector(this,p)
+    def dist (p: Point)   = (this - p).norm
+    def /    (f: Float  ) = Point(x/f,y/f)
+    override def toString = "(%.2f,%.2f)".format(x,y)
+    override def equals(that: Any) = that.isInstanceOf[Point] && { 
+	    val p = that.asInstanceOf[Point]
+	    Point.cmp(x,p.x) && Point.cmp(y,p.y)
+	}
+}
+
+class Vector(val p: Point, val q: Point) extends Point(q.x - p.x,q.y - p.y) {
+	def this(x: Float, y: Float) = this(Point(),Point(x,y))
+    def norm                     = Math.sqrt(x*x + y*y)
+    def cross(p: Point)          = x*p.x + y*p.y
+    def mid                      = (p + q)/2
+}
+
+object Point {
+    val EPSILON                       = 0.001
+    def apply(x: Float=0, y: Float=0) = new Point(x,y)
+    def cmp  (x: Float  , y: Float=0) = Math.abs(x - y) < EPSILON
+}
+
+object Vector {
+    def apply(x: Float=0, y: Float=0) = new Vector(x,y)
+    def apply(p: Point  , q: Point  ) = new Vector(p,q)
+}
---
+++ codeeval/moderate/FindASquare/main.py
@@ -0,0 +1,64 @@
+﻿import sys
+import re
+import math
+
+# We will use a general algorithm working also with pointing float coordinates
+EPSILON = 0.001
+
+def cmp(d0,d1=0):
+	return abs(d0 - d1) < EPSILON
+
+class vector:
+	def __init__(self,p0=0,p1=0):
+		(self.x,self.y) = (p1.x - p0.x,p1.y - p0.y) if type(p0) is vector and type(p1) is vector else (p0,p1)
+
+	def __sub__(self,v):
+		return vector(v,self)
+	
+	def __add__(self,v):
+		return vector(-self,v)
+				
+	def __neg__(self):
+		return vector(self,vector())
+	
+	def dist(self,v):
+		w = v - self
+		return math.sqrt(w.x*w.x + w.y*w.y)	
+		
+	def __eq__(self,v):
+		return self.x == v.x and self.y == v.y
+
+	@staticmethod
+	def mid(v,w):
+		m = v + w
+		return vector(m.x/2,m.y/2)
+
+	def norm(self):
+		return self.dist(vector())
+		
+	def cross(self,v):
+		return self.x*v.x + self.y*v.y
+		
+	def __str__(self):
+		return "({:.2f},{:.2f})".format(self.x,self.y)
+		
+	def __repr__(self):
+		return self.__str__()
+
+with open(sys.argv[1],'r') as lines:
+	for line in lines:
+		pts = [ vector(int(a),int(b)) for (a,b) in re.findall("\((\d+),(\d)+\)",line) ]
+
+		# we choose arbitrarily two lines with the 4 points. The lines may be
+		# parallel, in that case we try to swap the order and get orthogonal lines
+		v0  = pts[1] - pts[0]
+		v1  = pts[3] - pts[2]
+		res = v0 != vector() and v1 != vector()
+
+		if res:			
+			if v0 == v1 or v0 == -v1:
+				(a,b) = ((0,2),(1,3)) if v0 == v1 else ((0,3),(1,2))
+				res   = pts[a[0]] - pts[a[1]] == pts[b[0]] - pts[b[1]]
+			else:
+				res   = cmp(v0.cross(v1)) and v0.norm() == v1.norm() and vector.mid(pts[1],pts[0]) == vector.mid(pts[3],pts[2])
+		print(str(res).lower())
