6bb9508ad6112f2c23f37b723534066ff3edb606
Dicee <courtinot.david@orange.fr>
2015-01-12 09:01:18 +0100

    Mistake

---
+++ codeeval/easy/MatrixRotation/main.py
@@ -0,0 +1,11 @@
+﻿import sys
+from math import sqrt
+with open(sys.argv[1], 'r') as lines:
+	for line in lines:
+		split = line.split(" ")
+		n     = int(sqrt(len(split)))
+		res   = []
+		for j in range(n):
+			for i in reversed(range(n)):
+				res.append(split[i*n + j].strip())
+		print(" ".join(res))
