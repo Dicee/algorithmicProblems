619fc8636e74377309cef4ae743fcb1cda7d0257
Courtinot David <dcourtin@zirconium.enseeiht.fr>
2015-01-13 15:39:36 +0100

    No comment

---
+++ codeeval/moderate/OverlappingRectangles/main.py
@@ -0,0 +1,10 @@
+import sys
+with open(sys.argv[1],'r') as lines:
+    for line in lines:
+        split     = [ int(i) for i in line.split(",") ]
+        (tla,tlb) = ((split[0],split[1]),(split[4],split[5]))
+        (bra,brb) = ((split[2],split[3]),(split[6],split[7]))
+
+        (tl0,br0) = (tlb,bra) if tla[0] <= tlb[0] else (tla,brb) 
+        (tl1,br1) = (tla,brb) if tla[1] <= tlb[1] else (tlb,bra)
+        print(str(tl0[0] < br0[0] and br1[1] < tl1[1])) 
---
+++ codeeval/moderate/ReverseGroups/main.py
@@ -0,0 +1,15 @@
+import sys
+with open(sys.argv[1],'r') as lines:
+    for line in lines:
+        split = line.split(";")
+        n     = int(split[1])
+        arr   = split[0].split(",")
+        res   = [ None ]*len(arr)
+
+        q     = len(arr) // n
+        for i in range(q):
+            res[i*n:(i+1)*n - 1] = reversed(arr[i*n:(i+1)*n])
+
+        res[q*n:] = arr[q*n:]
+        print(",".join(res))
+
