3c9d54f9ee25035b70eae2084549220357c3811e
Dicee <courtinot.david@orange.fr>
2015-01-16 19:57:51 +0100

    One moderate problem

---
+++ codeeval/moderate/Sudoku/Main.scala
@@ -0,0 +1,53 @@
+﻿package testouille
+
+import scala.util.matching.Regex
+import scala.collection.immutable.Stack
+
+//import scala.collection.mutable.ArrayBuffer
+//
+//object Main extends App {
+//    scala.io.Source.fromFile(args(0)).getLines
+//    	.filter(!_.isEmpty)
+//        .map(line => {
+//            val nodes = line.split(",").map(s => new Node(s(0),s.last)).toSeq
+//            val paths = collection.mutable.HashMap[Node,Int]()
+//            for (node <- nodes) { paths += node -> 0; node.successors ++= nodes.filter(_.entry == node.exit) }
+//            
+//            nodes.map(_.successors)
+//        })
+//        .foreach(println)
+//}
+//
+//class Node(val entry: Char, val exit: Char) {
+//    var successors = ArrayBuffer[Node]()
+//    override def toString = "(%s,%s)".format(entry,exit)
+//}
+
+object Main extends App {
+    def getRow(grid: Array[Int], i: Int, n: Int) = for (j <- Range(0,n)) yield grid(i*n + j) 
+    def getCol(grid: Array[Int], j: Int, n: Int) = for (i <- Range(0,n)) yield grid(i*n + j)
+    
+    def getSquare(grid: Array[Int], i: Int, j:Int, n: Int) = {
+        val m = Math.sqrt(n).toInt
+        for (k <- Range(i*m,(i + 1)*m) ; l <- Range(j*m,(j + 1)*m)) yield grid(k*n + l)
+    }
+   
+    def isValid(it: Iterable[Int]) = { val set = it.toSet; set.size == it.size && set.forall(x => 1 <= x  && x <= it.size*it.size) }
+    
+    def checkGrid(grid: Array[Int], n: Int): String = {
+        val m = Math.sqrt(n).toInt
+        for (i <- Range(0,m) ; j <- Range(0,m)) 
+           if (!isValid(getRow(grid,i*m + j,n)) || !isValid(getCol(grid,i*m + j,n)) || !isValid(getSquare(grid,i,j,n)))
+                   return "False"
+        return "True"
+    }
+        
+    scala.io.Source.fromFile(args(0)).getLines
+    	.filter(!_.isEmpty)
+        .map(line => {
+        	val grid = line.substring(line.indexOf(";") + 1).split(",").map(_.toInt)
+        	val n    = Math.sqrt(grid.length).toInt
+        	checkGrid(grid,n)
+        })
+        .foreach(println)
+}
---
+++ codeeval/moderate/Sudoku/main.py
@@ -0,0 +1,44 @@
+﻿import sys
+import math
+with open(sys.argv[1],'r') as lines:
+	def getRow(grid,i):
+		return [ grid[i][j] for j in range(0,len(grid)) ]
+			
+	def getCol(grid,j):
+		return [ grid[i][j] for i in range(0,len(grid)) ]
+		
+	def cartesian_product(range0,range1):
+		res = []
+		for i in range0:
+			for j in range1:
+				res.append((i,j))
+		return res
+		
+	def getSquare(grid,i,j):
+		length = int(math.sqrt(len(grid)))
+		return [ grid[k][l] for (k,l) in cartesian_product(range(i*length,(i + 1)*length),range(j*length,(j + 1)*length)) ]
+		
+	def isValid(block):
+		to_set = set(block)
+		return len(to_set) == len(block) and all(1 <= i and i <= len(block) for i in to_set)
+		
+	for line in lines:
+		split = line.split(";")
+		n     = int(split[0])
+		grid  = [None]*n
+		
+		for i,x in enumerate([ int(y) for y in split[1].split(",") ]):
+			if (i % n == 0):
+				grid[i//n] = [0]*n
+			grid[i//n][i % n] = x
+			
+		m   = int(math.sqrt(len(grid)))
+		res = True
+		for i in range(m):
+			for j in range(m):
+				res = all(isValid(block) for block in [ getRow(grid,i*m + j),getCol(grid,i*m + j),getSquare(grid,i,j) ])
+				if not res:
+					break
+			if not res:
+				break
+		print(res)
