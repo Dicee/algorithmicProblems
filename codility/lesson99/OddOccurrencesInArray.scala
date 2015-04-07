dc39ea3bc7c9e5ab90db13d130fd96447e9d0e3f
David Courtinot <courtino@amazon.com>
2015-04-07 16:56:18 +0100

    Added a codility painless problem

---
+++ codility/lesson99/OddOccurrencesInArray.scala
@@ -0,0 +1,7 @@
+object Solution extends App {
+    def solution(arr: Array[Int]): Int = {
+        val numbers = scala.collection.mutable.HashMap[Int,Int]()
+        for (x <- arr) numbers += x -> ((numbers.getOrElse(x,0) + 1) % 2)
+        numbers.filter { case (_,v) => v == 1 }.keySet.last
+    }
+}
