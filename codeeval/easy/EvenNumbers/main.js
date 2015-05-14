d365c29cd3d7a150e8ca0f664f483f53649494ed
Dicee <dcourtinot@gmail.com>
2015-05-14 02:13:24 +0100

    Blablabla

--- codeeval/easy/EvenNumbers/main.js
+++ codeeval/easy/EvenNumbers/main.js
@@ -1,6 +1,4 @@
-package codeeval.easy.EvenNumbers;
-
-var fs  = require("fs");
+﻿var fs  = require("fs");
 fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
     console.log((parseInt(line) + 1) % 2);
 });
--- codeeval/easy/SumOfIntegersFromFile/main.js
+++ codeeval/easy/SumOfIntegersFromFile/main.js
@@ -1,6 +1,4 @@
-package codeeval.easy.SumOfIntegersFromFile;
-
-var fs  = require("fs");
+﻿var fs  = require("fs");
 sum     = 0
 fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
 	if (line.length) sum += parseInt(line)
