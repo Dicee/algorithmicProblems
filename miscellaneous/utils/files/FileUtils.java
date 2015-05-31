a7c20f590a026a3420272988f83f3790796b086a
Dicee <Dicee@users.noreply.github.com>
2015-05-31 21:33:51 +0100

    Added some utilities

--- miscellaneous/utils/files/FileUtils.java
+++ miscellaneous/utils/files/FileUtils.java
@@ -3,6 +3,8 @@ package miscellaneous.utils.files;
 import java.io.File;
 
 public final class FileUtils {
+	private FileUtils() { }
+	
 	public static String getResourceAbsolutePath(String localPath, Class<?> clazz) {
 		return clazz.getResource(localPath).getPath().substring(1);
 	}
@@ -13,4 +15,26 @@ public final class FileUtils {
 	 * @return canonical path corresponding to $code{path}
 	 */
 	public static String toCanonicalPath(String path) { return path.replace(File.separatorChar,'/'); }
+	
+	public static boolean hasExtension(String path, List<String> extensions) {
+		return hasExtension(new File(path),extensions);
+	}
+	
+	public static boolean hasExtension(File file, List<String> extensions) {
+		return extensions.stream().filter(file.getName()::endsWith).findAny().isPresent();
+	}
+	
+	public static File toExtension(String path, String extension) {
+		return toExtension(new File(path),extension);
+	}
+	
+	public static File toExtension(File file, String extension) {
+		if (hasExtension(file,Arrays.asList(extension))) return file;
+		
+		if (!extension.startsWith(".")) extension = "." + extension;
+		
+		String path = file.getAbsolutePath();
+		int lastDot = path.lastIndexOf('.');
+		return new File((lastDot < 0 ? path : path.substring(0,lastDot)) + extension);
+	}
 }
