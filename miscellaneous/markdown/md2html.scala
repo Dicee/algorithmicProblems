ebb87ab19c6da04ace27a45484cae83eba80c0a2
David Courtinot <courtino@amazon.com>
2015-06-02 18:18:30 +0100

    Added inline code formatting

--- miscellaneous/markdown/md2html.scala
+++ miscellaneous/markdown/md2html.scala
@@ -1,64 +1,129 @@
 package miscellaneous.markdown
 
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
+import scala.io.Source
+import scala.collection.JavaConversions
 import java.io.File
-import java.io.PrintWriter
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.nio.file.StandardCopyOption
-import miscellaneous.utils.files.FileUtils
+import scala.collection.mutable.LinkedHashSet
+import miscellaneous.utils.check.Check
 
-object md2html extends App {
-//	interesting themes : base16-dark.css, eclipse.css, mbo.css, monokai.css, pastel-on-dark.css
-	private val DEFAULT_THEME = Themes.AMAZON_LIGHT
-		
-	try {
-		if (args.length == 0) exitWithError("missing input file name")
+object MarkdownParser {
+	val doc = new Document
+	
+	def parse(file: File) = {
+		doc.includeCss("http://codemirror.net/theme/%s.css".format(System.getProperty("editorTheme")))
+		doc.includeJsOrCss(
+				"""<link rel="stylesheet" href="style.css">""", //
+				"""<link rel="stylesheet" href="http://codemirror.net/lib/codemirror.css">""", //
+				"""<script src="http://codemirror.net/lib/codemirror.js"></script>""")
+				
+		optimizeBr(HTMLNode.parse(Source.fromFile(file).getLines.toList))
+        	.map(_.elt)
+        	.foreach(doc.appendChild(_))
+        doc.includeRawCss(".CodeMirror {\n\tborder: 1px solid #eee;\n\theight: auto;\n}")
+        doc
+	}
+        	
+    private def optimizeBr(nodes: List[HTMLNode]): List[HTMLNode] = nodes match {
+        case (s1: Span) :: Br() :: (s2: Span) :: q => s1 :: Br() :: s2 :: optimizeBr(q)
+        case (s1: Span) :: (s2: Span)         :: q => s1 :: Br() :: s2 :: optimizeBr(q)
+        case Br()       :: node               :: q => node             :: optimizeBr(q)
+        case node       :: Br()               :: q => node             :: optimizeBr(q)
+        case t                                :: q => t                :: optimizeBr(q)
+        case Nil                                   => Nil
+    }
+	
+	private abstract class HTMLNode(val elt: Element) {
+        def appendHTML(htmlContent: String) =
+            JavaConversions.asScalaIterator(Jsoup.parseBodyFragment(htmlContent).body.childNodes.iterator).toList.foreach(elt.appendChild(_))
+    }
 
-        val path = FileUtils.toCanonicalPath(args(0))
-	    if (!checkExtension(path)) exitWithError("the input file should have .md or .mardown extension")
+	private object HTMLNode {
+        val MAX_TITLE_LEVEL = 6
+        
+        private implicit class AugmentedString(s: String) {
+        	val str = s.trim
+        	
+	        def hasPrefix (prefix : Char  ) = str.length > 0 && str(0) == prefix && (str.length == 1 || str(1) != prefix)  
+	        def trim      (margin : Int   ) = str.drop(margin).dropRight(margin)
+	        def wrapBy    (wrapper: String) = wrapper + str + wrapper
+	        def wrapByTag(tagName: String)  = s"<$tagName>$str</$tagName>"
+	       
+	        def isTitle    = str(0) == '#'
+	        def isListItem = str.hasPrefix('-')
+	        def isCode     = str.startsWith("```")
+	        def isBlank    = str.isEmpty
+    	}
+        
+	    def parse(lines: List[String]): List[HTMLNode] = {
+	        lines match {
+	        	case t :: q if t.isCode =>
+	                val language = t.drop(3).trim
+	                val content  = q.takeWhile(!_.isCode)
+	                Code(content, Code.getMime(language)) :: parse(q.drop(content.length + 1))
+	                   
+	            case t :: q if t.isListItem =>
+	                val items = lines.takeWhile(s => !s.isEmpty && s.isListItem).map(parseListItem)
+	                Ul(items) :: parse(lines.drop(items.length))
+	                   
+	            case t :: q => parseSingleLine(t) :: parse(q)
+	            case Nil    => Nil
+	        }
+	    }
+	       
+	    private def parseSingleLine(s: String): HTMLNode = if (s.isBlank   ) Br()             else parseNonEmpty (s)
+	    private def parseNonEmpty  (s: String): HTMLNode = if (s.isListItem) parseListItem(s) else tryTitle      (s)
+	    private def tryTitle       (s: String): HTMLNode = if (s.isTitle   ) parseTitle   (s) else parseParagraph(s)
+	              
+	    private def parseListItem(s: String) = Li(parseStyledText(s.substring(1).trim))
+	    private def parseTitle(s: String) = {
+	    	val suffix = s.takeWhile(_ == '#')
+	    	val tagName = "h%s".format(Math.min(MAX_TITLE_LEVEL,suffix.length))
+	        H(parseStyledText(s.dropWhile(_ == '#').trim), Math.min(MAX_TITLE_LEVEL,suffix.length))
+	    }
+	      
+	    private def parseParagraph(s: String) = P(parseStyledText(s))
+	    private def parseStyledText(s: String)  = s
+	        .replaceAll("\\s+" .wrapBy("(\\*){2}"),""                                       )
+	        .replaceAll("\\s+" .wrapBy("\\*"     ),""                                       )
+	        .replaceAll("\\s+" .wrapBy("~~"      ),""                                       )
+	        .replaceAll("(.*?)".wrapBy("(\\*){2}"),"<span class=\"important\">$2</span>"    )
+	        .replaceAll("(.*?)".wrapBy("\\*"     ),"<span class=\"emphasize\">$1</span>"    )
+	        .replaceAll("(.*?)".wrapBy("~~"      ),"<span class=\"strikethrough\">$1</span>")
+            .replaceAll("(.*?)".wrapBy("`"       ),"<code class=\"inline-code\">$1</code>"  )
+	}
 	    
-	    val options  = DEFAULT_THEME.toMap ++ parseOptions(args.toList.drop(1).map(_.trim)) 
-	    val theme    = Theme(options("name"),options("editorTheme"),options("logo"),options("style"))
-	    System.setProperty("editorTheme",theme.editorTheme)
-	    
-		val doc      = MarkdownParser.parse(new File(path))
-	    val htmlFile = new File(toHTMLextension(path))
-	    val pw       = new PrintWriter(htmlFile)
-	    pw.write(doc.html)
-	    pw.close()     
-	    
-	    val destPath = new File(path).getAbsoluteFile.getParent + File.separator
-	    copyFiles(theme.logo,destPath + "logo.png" )
-	    copyFiles(theme.css ,destPath + "style.css")
-	} catch {
-		case t: Throwable => exitWithError(t.getMessage)
-	}
-	
-	private type Options = Map[String,String]
-	
-	private def checkExtension (path: String)                = path.endsWith(".md") || path.endsWith(".markdown")
-	private def toHTMLextension(path: String)                = path.substring(0,path.lastIndexOf('.')) + ".html"
-	private def copyFiles(srcPath: String, destPath: String) = Files.copy(Paths.get(srcPath),Paths.get(destPath),StandardCopyOption.REPLACE_EXISTING)	
-
-	private def parseOptions   (args: List[String]): Options = args match {
-		case key :: value :: q if key.startsWith("--") => parseOptions(q) ++ parseOption(key.drop(2),value)
-		case key :: value :: q                         => exitWithError("syntax error in the command")
-		case key :: Nil        if isValidOption(key)   => exitWithError("option " + key.drop(2) + " has no assigned value")
-		case key :: Nil                                => exitWithError("unexpected token " + key)
-		case Nil                                       => Map()
-	}
-	
-	private lazy val VALID_OPTIONS                      = Set("theme", "editorTheme", "logo", "style")
-	private def isValidOption(s: String)                = { s.startsWith("--") && VALID_OPTIONS.contains(s.drop(2)) }
-	private def parseOption(key: String, value: String) = {
-		if (!VALID_OPTIONS.contains(key)) exitWithError(s"$key is not a valid option")	
-		if (key == "theme") Themes(value).toMap else Map(key.drop(2) -> value)
-	}
-	
-	private def exitWithError(msg: String)              = { println(s"Error: $msg"); printUsage; exit }
-	private def exit                                    = { System.exit(-1); ??? }
-	private def printUsage                              = {
-		println("SYNTAX: md2html <input> [options...]")
-		println("OPTIONS: \n\t--theme <theme>\n\t--logo <path>\n\t--editorTheme <editorTheme>\n\t--style <path>")
-	}
+    private case class Br  (                                 ) extends HTMLNode(doc.create("br"       ))
+    private case class Li  (text : String                    ) extends HTMLNode(doc.create("li"       )) { appendHTML(text )                                }
+    private case class Span(text : String                    ) extends HTMLNode(doc.create("span"     )) { appendHTML(text )                                }
+    private case class H   (title: String, level: Int        ) extends HTMLNode(doc.create("h" + level)) { appendHTML(title)                                }
+    private case class P   (text : String                    ) extends HTMLNode(doc.create("p"        )) { appendHTML(text )                                }
+    private case class Ul  (items: List[Li]                  ) extends HTMLNode(doc.create("ul"       )) { items.foreach(item => elt.appendChild(item.elt)) }
+    private case class Code(lines: List[String], mime: String) extends HTMLNode(doc.create("div"      )) {
+    	appendHTML(Code.TEMPLATE
+    			.replace("${code}",lines.mkString("\n","\n","\n"))
+    			.replace("${theme}",System.getProperty("editorTheme"))
+    			.replace("${mime}",mime)
+    			.replace("${id}",Code.updateId.toString))
+    	doc.includeJs("http://codemirror.net/mode/%s".format(Code.MIMES(mime)))
+    }
+    private object Code {
+    	private val TEMPLATE = Source.fromURL(Code.getClass.getResource("resources/code-template.html")).getLines.mkString("\n")
+		private val MIMES = Map(
+				"text/x-java"   -> "clike/clike.js", "text/x-c++src" -> "clike/clike.js", "text/x-csrc"     -> "clike/clike.js", //
+				"text/x-scala"  -> "clike/clike.js", "text/x-stex"   -> "stex/stex.js"  , "text/javascript" -> "javascript/javascript.js", //
+				"text/x-python" -> "python/python.js" //
+		)
+    	
+    	private val LANGUAGES = Map (
+			"java"  -> "text/x-java" , "cpp" -> "text/x-c++src", "c++"        -> "text/x-c++src"  , "c"       -> "text/x-csrc", //
+			"scala" -> "text/x-scala", "tex" -> "text/x-stex"  , "javascript" -> "text/javascript",  "python" -> "text/x-python"  // 
+		)
+    			
+		private var id = -1
+		def updateId = { id += 1; id }
+    	
+    	def getMime(mime: String) = LANGUAGES.getOrElse(mime, LANGUAGES("java"))
+    }
 }
