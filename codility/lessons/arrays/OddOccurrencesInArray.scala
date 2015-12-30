38c63649947e682d0d8536448f36fb536337b22b
Dicee <Dicee@users.noreply.github.com>
2015-12-30 23:06:57 +0000

    Shorter version, and more efficient

--- codility/lessons/arrays/OddOccurrencesInArray.scala
+++ codility/lessons/arrays/OddOccurrencesInArray.scala
@@ -1,9 +1,5 @@
 package codility.lessons.arrays;
 
 object SolutionScala extends App {
-    def solution(arr: Array[Int]): Int = {
-        val numbers = scala.collection.mutable.HashMap[Int,Int]()
-        for (x <- arr) numbers += x -> ((numbers.getOrElse(x,0) + 1) % 2)
-        numbers.filter { case (_,v) => v == 1 }.keySet.last
-    }
+    def solution(arr: Array[Int]) = arr.reduce(_ ^ _)
 }
