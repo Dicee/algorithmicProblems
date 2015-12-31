8c2ed5ad694e8b76d091a6911fce0b0856821b5a
Dicee <dcourtinot@gmail.com>
2015-12-31 03:11:27 +0000

    Improved a solution to a Codeeval problem and deleted a duplicate of this solution

--- codeeval/moderate/PascalsTriangle/Main.scala
+++ codeeval/moderate/PascalsTriangle/Main.scala
@@ -1,23 +1,15 @@
 package codeeval.moderate.PascalsTriangle;
 
+import scala.io.Source
+
 object Main extends App {
-	def pascalSeries(n: Int, sb: StringBuilder): List[Int] = {
-		if (n == 1)	{ 
-			sb.append("1 ")
-			List(1) 
-		} else {
-			val series = pascalSeries(n-1,sb)
-			val res    = (series :+ 0) zip (0 :: series) map { case (a,b) => a + b }
-			res.addString(sb,""," "," ")
-			res
-		}
-	}
-	
-	scala.io.Source.fromFile(args(0)).getLines
-		.map(line => {
-			val sb     = new StringBuilder()
-			pascalSeries(line.toInt,sb)
-			sb
-		})
-		.foreach(println)
+	def pascalSeries(n: Int, sb: StringBuilder): List[Int] = n match {
+        case 1 => sb.append("1 "); List(1)
+        case _ => 
+            val series = pascalSeries(n - 1, sb)
+            val res    = (series :+ 0) zip (0 :: series) map { case (a, b) => a + b }
+            res.addString(sb, "", " ", " ")
+            res
+    }
+	Source.fromFile(args(0)).getLines.map(line => { val sb = new StringBuilder(); pascalSeries(line.toInt, sb); sb }).foreach(println)
 }
