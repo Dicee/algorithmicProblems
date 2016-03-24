7497d49289b83f48a404b066f2857944c90d1d9c
Dicee <dcourtinot@gmail.com>
2016-03-24 16:09:46 +0100

    Making Intellij happy

--- codeeval/easy/AgeDistribution/Main.scala
+++ codeeval/easy/AgeDistribution/Main.scala
@@ -1,14 +1,5 @@
 package codeeval.easy.AgeDistribution;
 
-object Main extends App {
-	scala.io.Source.fromFile(args(0)).getLines.filter(!_.isEmpty)
-    	.map(n => AgeCategory.values.find(_.accept(n.toInt)) match {
-    	    case Some(x) => x.msg
-    	    case None    => "This program is for humans"
-    	})
-    	.foreach(println)
-}
-
 object AgeCategory  {
     val _1     = AgeCategoryVal(0 ,2  ,"Still in Mama's arms"      )
     val _2     = AgeCategoryVal(3 ,4  ,"Preschool Maniac"          )
@@ -20,6 +11,15 @@ object AgeCategory  {
     val _8     = AgeCategoryVal(66,100,"The Golden Years"          )
     val values = Array(_1,_2,_3,_4,_5,_6,_7,_8)
     protected case class AgeCategoryVal(low: Int, high: Int, msg: String) {
-        def accept(n: Int) = low <= n && n <= high 
+        def accept(n: Int) = low <= n && n <= high
     }
+}
+
+object Main extends App {
+	scala.io.Source.fromFile(args(0)).getLines.filter(!_.isEmpty)
+    	.map(n => AgeCategory.values.find(_.accept(n.toInt)) match {
+    	    case Some(x) => x.msg
+    	    case None    => "This program is for humans"
+    	})
+    	.foreach(println)
 }
