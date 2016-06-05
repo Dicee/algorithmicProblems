9c816b334cc159a37b6cbe0c257a7ad6d8df1d10
Dicee <dcourtinot@gmail.com>
2016-06-05 15:50:32 +0100

    Added a partial solution for a Codility problem

---
+++ codility/lessons/dynamicProgramming/MinAbsSum_memoizedRecursiveFunction.scala
@@ -0,0 +1,26 @@
+package codility.lessons.dynamicProgramming
+
+/**
+ * Level: ambitious
+ *
+ * Note: this solution does not meet all the performance requirements, it either fails to terminate
+ *       before the timeout or crashes due to a stack overflow
+ */
+object MinAbsSum_memoizedRecursiveFunction {
+  def solution(arr: Array[Int]): Int = {
+    val positiveArr = arr.map(Math.abs)
+
+    val cache = scala.collection.mutable.Map[(Int, Int), Int]()
+    def recSol(i: Int, currentCost: Int = 0): Int = {
+      cache.getOrElseUpdate((i, currentCost),
+        if (i >= positiveArr.length) currentCost
+        else {
+          val rec1 = recSol(i + 1, currentCost + positiveArr(i))
+          val rec2 = recSol(i + 1, currentCost - positiveArr(i))
+          Math.min(Math.abs(rec1), Math.abs(rec2))
+        }
+      )
+    }
+    recSol(0)
+  }
+}
