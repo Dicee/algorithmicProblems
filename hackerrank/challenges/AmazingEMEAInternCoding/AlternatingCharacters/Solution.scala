2fcbabf59d65aedece937180e32f3e5764880380
Dicee <dcourtinot@gmail.com>
2016-08-14 00:26:26 +0100

    Improve solution

--- hackerrank/challenges/AmazingEMEAInternCoding/AlternatingCharacters/Solution.scala
+++ hackerrank/challenges/AmazingEMEAInternCoding/AlternatingCharacters/Solution.scala
@@ -7,10 +7,10 @@ import scala.io.Source
 // https://www.hackerrank.com/contests/amazing-intern-coding-challenge-2/challenges/alternating-characters
 object Solution {
     def main(args: Array[String]) {
-        println(Source.stdin.getLines.toList.tail
-                      .map(_.toList)
-                      .map(chars => chars.tail.zip(chars)
-                      .count(t => t._1 == t._2))
-                      .mkString("\n"))      
+        Source.stdin.getLines.toList.tail
+              .map(_.toList)
+              .map(chars => chars.tail.zip(chars)
+              .count(t => t._1 == t._2))
+              .foreach(println)
     }
 }
