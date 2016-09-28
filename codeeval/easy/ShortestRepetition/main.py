62dbba66f477edd0b45a87aa6914e0bb558a54b0
Dicee <Dicee@users.noreply.github.com>
2016-09-28 22:55:06 +0100

    Removed unused variable. Ah... reading your old code is always an experience

--- codeeval/easy/ShortestRepetition/main.py
+++ codeeval/easy/ShortestRepetition/main.py
@@ -3,12 +3,11 @@ import re
 
 lines = open(sys.argv[1], 'r')
 for line in lines:
-	(end,count,res) = (0,0,1)
+	(count,res) = (0,1)
 	for i,ch in enumerate([ x for x in line[1:] if re.match(r"[^\n ]",x) ]):
 		if ch != line[count]:
 			count = 0
 			res  += 1
-			end  += 1
 		else:
 			count += 1
 	print(res)
