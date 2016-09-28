c3cddaa1ba22e6d1b6d11bdd012e7c33cf395907
Dicee <Dicee@users.noreply.github.com>
2016-09-28 23:04:24 +0100

    Replace twisted regex by simple string trimming

--- codeeval/easy/ShortestRepetition/main.py
+++ codeeval/easy/ShortestRepetition/main.py
@@ -1,10 +1,9 @@
 import sys
-import re
 
 lines = open(sys.argv[1], 'r')
 for line in lines:
 	(count,res) = (0,1)
-	for i,ch in enumerate([ x for x in line[1:] if re.match(r"[^\n ]",x) ]):
+	for i,ch in enumerate(line.strip()[1:]):
 		if ch != line[count]:
 			count = 0
 			res  += 1
