3ea729a9d2bc7f1b8cf897d7509be52ef4392c40
Dicee <Dicee@users.noreply.github.com>
2017-10-01 23:23:48 +0100

    Another Interviewbit question

---
+++ interviewbit/Programming/Strings/Atoi/AddBinaryString/Solution.java
@@ -0,0 +1,22 @@
+import static java.math.BigInteger.ZERO;
+
+import java.math.BigInteger;
+
+public class Solution {
+    public String addBinary(String a, String b) {
+        BigInteger n = parseBinary(a).add(parseBinary(b));
+
+        BigInteger two = BigInteger.valueOf(2);
+        StringBuilder sb = new StringBuilder();
+        do {
+            sb.append(n.mod(two));
+            n = n.shiftRight(1);
+        } while (n.compareTo(ZERO) > 0);
+        return sb.reverse().toString();
+    }
+
+    // you call that cheating ? Well ok that's cheating, but it's simple :)
+    private static BigInteger parseBinary(String s) {
+        return new BigInteger(s, 2);
+    }
+}
