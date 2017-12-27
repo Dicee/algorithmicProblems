e653e1efc33be806491ac2bdb5aab068c1f56b99
Dicee <dcourtinot@gmail.com>
2017-12-27 13:48:16 +0000

    Another HackerRank problem

---
+++ hackerrank/algorithms/implementation/cavityMap/Solution.scala
@@ -0,0 +1,34 @@
+package hackerrank.algorithms.implementation.cavityMap
+
+// Difficulty: trivial although lengthy
+
+// https://www.hackerrank.com/challenges/cavity-map/problem
+object Solution {
+  def main(args: Array[String]) {
+    val lines  = scala.io.Source.stdin.getLines()
+    val n      = lines.next().toInt
+    val matrix = Array.ofDim[Int](n, n)
+
+    for {
+      i <- 0 until n
+      values = lines.next().split("").view.map(_.toInt)
+      (v, j) <- values.zipWithIndex
+    } matrix(i)(j) = v
+
+    for (i <- 0 until n; j <- 0 until n) {
+      print(if (isCavity(i, j, matrix)) "X" else matrix(i)(j))
+      if (j == n - 1) println
+    }
+  }
+
+  private def isCavity(i: Int, j: Int, matrix: Array[Array[Int]]) = {
+    val maxIndex = matrix.length - 1
+    if (i == 0 || j == 0 || i == maxIndex || j == maxIndex) false
+    else (for {
+      di <- -1 to 1; dj <- -1 to 1
+      if di == 0 ^ dj == 0
+      ii = i + di; jj = j + dj
+      if 0 <= ii && 0 <= jj && ii < matrix.length && jj < matrix.length
+    } yield matrix(ii)(jj)).forall(_ < matrix(i)(j))
+  }
+}
