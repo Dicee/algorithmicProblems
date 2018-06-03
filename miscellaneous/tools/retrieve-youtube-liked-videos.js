5be9ce886d68f7c8f9006056e28c2a5a80d83fa4
Dicee <dcourtinot@gmail.com>
2018-06-03 18:21:58 +0100

    Not an "algorithmic problem" per se, but adding a clunky tool allowing to list all currently liked videos for a Youtube account. This allows knowing which videos were deleted by Youtube and be able to find an alternate version of them instead of just being left with a 'Deleted video' marker.

---
+++ miscellaneous/tools/retrieve-youtube-liked-videos.js
@@ -0,0 +1,34 @@
+scrollUntilReachedBottom(printLikedVideos);
+
+function scrollUntilReachedBottom(onScrolledToBottom) {
+	window.scrollBy(0, 10000);
+
+	const currentVideosCount = getVideoListElements().length;
+	setTimeout(() => {
+		const newVideosCount = getVideoListElements().length;
+		if (newVideosCount > currentVideosCount) scrollUntilReachedBottom(onScrolledToBottom);
+		else onScrolledToBottom();
+	}, 2000)
+}
+
+function printLikedVideos() {
+	const likedVideos = [];
+
+	for (const videoListElement of getVideoListElements()) {
+		const blockLink = videoListElement.children[1].children[0];
+		
+		const title = blockLink.children[1].children[0].children[1].innerHTML.trim();
+		const link = blockLink.href;
+
+		likedVideos.push({
+			'title': title,
+			'link': link
+		});
+	}
+
+	console.log(JSON.stringify(likedVideos));
+}
+
+function getVideoListElements() {
+	return document.getElementById("contents").children[0].children[2].children[0].children[0].children;
+}
