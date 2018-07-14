8cfc851f8d19758af525d934f7cf7b37417eadd1
Dicee <Dicee@users.noreply.github.com>
2018-07-14 10:03:23 +0100

    Edit comment

--- hackerrank/algorithms/dynamicProgramming/maxArraySum/Solution.scala
+++ hackerrank/algorithms/dynamicProgramming/maxArraySum/Solution.scala
@@ -2,9 +2,9 @@ package hackerrank.algorithms.dynamicProgramming.maxArraySum
 
 import scala.collection.mutable
 
-// Difficulty: pretty easy, but my solution is far from optimal. I used straightforward memoization which would probably have not worked for larger problems 
-//             (would require bottom-up solving rather than top-bottom). I also saw other solutions afterwards involving a single loop and using only two
-//             integers of memory.
+// Difficulty: pretty easy, but the first solution I came up with was far from optimal. I used straightforward memoization 
+//             which would probably have not worked for larger problems. Bottom-up solving rather than top-bottom allows solving 
+//             any size of this problem and in fact isn't more complicated to implement for this particular problem.
 
 // https://www.hackerrank.com/challenges/max-array-sum/problem?h_l=playlist&slugs%5B%5D%5B%5D=interview&slugs%5B%5D%5B%5D=interview-preparation-kit&slugs%5B%5D%5B%5D=dynamic-programming
 object Solution {
