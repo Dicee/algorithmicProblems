b13729a6020e3be11093968a82ac76a5f0b7aa90
Dicee <Dicee@users.noreply.github.com>
2018-07-14 09:43:17 +0100

    Simplify solution

--- hackerrank/algorithms/dynamicProgramming/maxArraySum/Solution.scala
+++ hackerrank/algorithms/dynamicProgramming/maxArraySum/Solution.scala
@@ -8,15 +8,13 @@ import scala.collection.mutable
 
 // https://www.hackerrank.com/challenges/max-array-sum/problem?h_l=playlist&slugs%5B%5D%5B%5D=interview&slugs%5B%5D%5B%5D=interview-preparation-kit&slugs%5B%5D%5B%5D=dynamic-programming
 object Solution {
-    def maxSubsetSum(arr: Array[Int]): Long = recSol(arr, 0, arr.length, new mutable.HashMap())
+    def maxSubsetSum(arr: Array[Int]): Long = recSol(arr, arr.length, new mutable.HashMap())
     
-    private def recSol(arr: Array[Int], start: Int, end: Int, memoize: mutable.Map[(Int, Int), Long]): Long = {
-        if (start >= end) Int.MinValue
-        else if (start + 1 >= end) Math.max(0, arr(start))
-        else if (start + 2 >= end) Math.max(0, Math.max(arr(start), arr(start + 1)))
+    private def recSol(arr: Array[Int], end: Int, memoize: mutable.Map[Int, Long]): Long = {
+        if (end <= 2) Math.max(0, arr.view.slice(0, end).max)
         else {
-            val takeLast = memoize.getOrElseUpdate((start, end - 2), sol(arr, start, end - 2, memoize)) + arr(end - 1)
-            val skipLast = memoize.getOrElseUpdate((start, end - 1), sol(arr, start, end - 1, memoize))
+            val takeLast = memoize.getOrElseUpdate(end - 2, recSol(arr, end - 2, memoize)) + arr(end - 1)
+            val skipLast = memoize.getOrElseUpdate(end - 1, recSol(arr, end - 1, memoize))
             Math.max(takeLast, skipLast)
         }
     }
