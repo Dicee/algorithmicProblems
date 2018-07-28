b6381d4ada0b11cbea79e105dded6141080ebb4d
Dicee <Dicee@users.noreply.github.com>
2018-07-28 12:14:21 +0100

    Another Hackerrank problem

---
+++ hackerrank/algorithms/strings/commonChild/Solution.scala
@@ -0,0 +1,19 @@
+import hackerrank.algorithms.strings.commonChild
+
+// Difficulty: trivial if you already know LCS/are trained with DP. A bit disappointing that I was able to copy-paste a fragment
+//             of my LCS solution (also from Hackerrank0 to solve this problem.
+
+// https://www.hackerrank.com/challenges/common-child/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=strings
+object Solution {
+    def commonChild(s1: String, s2: String): Int = {
+       val matrix = Array.ofDim[Int](s1.length + 1, s2.length + 1)
+       for (i <- 1 until matrix.length) {
+            for (j <- 1 until matrix(0).length) {
+                if (s1(i - 1) == s2(j - 1)) {
+                    matrix(i)(j) = 1 + matrix(i - 1)(j - 1)
+                } else matrix(i)(j) = Math.max(matrix(i)(j - 1), matrix(i - 1)(j))
+            }
+        }
+        matrix(s1.length)(s2.length)
+    }
+}
