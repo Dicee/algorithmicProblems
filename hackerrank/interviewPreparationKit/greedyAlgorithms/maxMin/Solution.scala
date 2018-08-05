d06eb489d9358af7a7dba52c3eb5d96823048108
Dicee <Dicee@users.noreply.github.com>
2018-08-05 15:29:49 +0100

    Hackerrank problem

---
+++ hackerrank/interviewPreparationKit/greedyAlgorithms/maxMin/Solution.scala
@@ -0,0 +1,9 @@
+// Difficulty: trivial
+
+// https://www.hackerrank.com/challenges/angry-children/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=greedy-algorithms
+object Solution {
+    def maxMin(k: Int, arr: Array[Int]): Int = {
+        val sortedValues = arr.view.sorted.toList
+        sortedValues.zip(sortedValues.drop(k - 1)).map { case (a, b) => b - a }.min
+    }
+}
