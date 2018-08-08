c24cfb1c462384bb57a0bc8959311794d9ecbc69
Dicee <Dicee@users.noreply.github.com>
2018-08-09 00:55:08 +0100

    Typo

--- hackerrank/algorithms/implementation/absolutePermutation/Solution.scala
+++ hackerrank/algorithms/implementation/absolutePermutation/Solution.scala
@@ -10,11 +10,11 @@ object Solution {
         if (searchSolution(result, n, k)) result else Array(-1)
     }
     
-    // Each element i can be moved to at most to positions (i + k and i - k). However, elements between 0 and k - 1
-    // and only be moved forward, which means that the element that is displaced by moving i (< k) to i + k is the only
-    // element that can fill the void left by i after it moved forward. Therefore, any solution *must* swap i and i + k.
-    // In the end, there is actually a single possible position for each element. In some cases, some elements will need 
-    // to be moved to the same position, rendering the problem impossible.
+    // Each element i can be moved to at most two positions (i + k and i - k). However, elements between 0 and k - 1 
+    // (for example)  and only be moved forward, which means that the element that is displaced by moving i (< k) to i + k 
+    // is the only element that can fill the void left by i after it moved forward. Therefore, any solution *must* swap i 
+    // and i + k. In the end, there is actually a single possible position for each element. In some cases, some elements 
+    // will need to be moved to the same position, rendering the problem impossible.
     def searchSolution(arr: Array[Int], n: Int, k: Int): Boolean = {
         for (i <- 0 until n) {
             // otherwise, the element has already been swapped
