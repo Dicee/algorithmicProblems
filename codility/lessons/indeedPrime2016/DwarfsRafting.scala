8dcab032b9ca62f1bebdda284f7f7236f9cbf8b9
Dicee <Dicee@users.noreply.github.com>
2018-08-29 23:14:14 +0100

    Simplify code a little bit

--- codility/lessons/indeedPrime2016/DwarfsRafting.scala
+++ codility/lessons/indeedPrime2016/DwarfsRafting.scala
@@ -4,42 +4,34 @@ package codility.lessons.indeedPrime2016
  * Level: painless
  */
 object DwarfsRafting {
-  def parseCoordinatesList(s: String) = if (s.isEmpty) Set[(Int, Int)]() else s.split(' ').map(parseCoordinates).toSet
-  def parseCoordinates    (s: String) = (s.substring(0, s.length - 1).toInt - 1, s.charAt(s.length - 1) - 'A')
-
   def solution(n: Int, barrelsString: String, dwarfsString: String): Int = {
     val barrels = parseCoordinatesList(barrelsString)
     val dwarfs  = parseCoordinatesList(dwarfsString)
 
-    def stateForSlice(topLeft: (Int, Int)) = {
-      val length = n / 2
-      val slice  = (for (i <- topLeft._1 until topLeft._1 + length; j <- topLeft._2 until topLeft._2 + length) yield (i, j)).toSet
+    def quarter(topLeft: (Int, Int)) = {
+      val (x, y) = topLeft
+      val slice  = (for (i <- x until x + n/2; j <- y until y + n/2) yield (i, j)).toSet
+      
       val existingDwarfs = (slice & dwarfs).size
       val barrelsCount   = (slice & barrels).size
 
-      State(existingDwarfs, slice.size - existingDwarfs - barrelsCount)
+      Quarter(existingDwarfs, slice.size - barrelsCount)
     }
-
-    val topLeft     = stateForSlice(0, 0)
-    val topRight    = stateForSlice(0, n/2)
-    val bottomRight = stateForSlice(n/2, n/2)
-    val bottomLeft  = stateForSlice(n/2, 0)
-
+    
     // simple maths show that top-left = bottom-right and top-right = bottom-left
-    val diagonalBalancedTotal     = findBalancedTotal(topLeft, bottomRight)
-    val antiDiagonalBalancedTotal = findBalancedTotal(topRight, bottomLeft)
-
-    if (diagonalBalancedTotal < 0 || antiDiagonalBalancedTotal < 0) -1
-    else diagonalBalancedTotal + antiDiagonalBalancedTotal - List(topLeft, topRight, bottomRight, bottomLeft).map(_.existingDwarfs).sum
+    val onboardedOnDiagonal     = maxOnboardable(quarter(0, 0), quarter(n/2, n/2))
+    val onboardedOnAntiDiagonal = maxOnboardable(quarter(0, n/2), quarter(n/2, 0))
+    if (onboardedOnDiagonal < 0 || onboardedOnAntiDiagonal < 0) -1 else onboardedOnDiagonal + onboardedOnAntiDiagonal
   }
 
-  def findBalancedTotal(state1: State, state2: State) = {
-    val balance = Math.min(state1.maxCapacity, state2.maxCapacity)
-    if (balance < Math.max(state1.existingDwarfs, state2.existingDwarfs)) -1
-    else                                                                   2 * balance
+  private def parseCoordinatesList(s: String) = if (s.isEmpty) Set[(Int, Int)]() else s.split(' ').map(parseCoordinates).toSet
+  private def parseCoordinates    (s: String) = (s.substring(0, s.length - 1).toInt - 1, s.last - 'A')
+  
+  private def maxOnboardable(q1: Quarter, q2: Quarter) = {
+    val maxBalancedCapacity = Math.min(q1.maxCapacity, q2.maxCapacity)
+    val minBalancedCapacity = Math.max(q1.existingDwarfs, q2.existingDwarfs)
+    if (maxBalancedCapacity < minBalancedCapacity) -1 else 2 * maxBalancedCapacity - q1.existingDwarfs - q2.existingDwarfs
   }
-
-  case class State(existingDwarfs: Int, seatsAvailable: Int) {
-    val maxCapacity = existingDwarfs + seatsAvailable
-  }
+  
+  private case class Quarter(existingDwarfs: Int, maxCapacity: Int)
 }
