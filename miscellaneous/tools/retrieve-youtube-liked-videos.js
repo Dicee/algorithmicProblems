61e11a5fab272e7c10a6cd5365f7faebf8522b6d
Dicee <Dicee@users.noreply.github.com>
2019-03-25 22:05:08 +0000

    Update scraping script (a few changes in Youtube's layout)

--- miscellaneous/tools/retrieve-youtube-liked-videos.js
+++ miscellaneous/tools/retrieve-youtube-liked-videos.js
@@ -1,34 +1,34 @@
 scrollUntilReachedBottom(printLikedVideos);
 
 function scrollUntilReachedBottom(onScrolledToBottom) {
-	window.scrollBy(0, 10000);
+    window.scrollBy(0, 10000);
 
-	const currentVideosCount = getVideoListElements().length;
-	setTimeout(() => {
-		const newVideosCount = getVideoListElements().length;
-		if (newVideosCount > currentVideosCount) scrollUntilReachedBottom(onScrolledToBottom);
-		else onScrolledToBottom();
-	}, 2000)
+    const currentVideosCount = getVideoListElements().length;
+    setTimeout(() => {
+     const newVideosCount = getVideoListElements().length;
+     if (newVideosCount > currentVideosCount) scrollUntilReachedBottom(onScrolledToBottom);
+     else onScrolledToBottom();
+    }, 2000)
 }
 
 function printLikedVideos() {
-	const likedVideos = [];
+    const likedVideos = [];
 
-	for (const videoListElement of getVideoListElements()) {
-		const blockLink = videoListElement.children[1].children[0];
-		
-		const title = blockLink.children[1].children[0].children[1].innerHTML.trim();
-		const link = blockLink.href;
+    for (const videoListElement of getVideoListElements()) {
+     const blockLink = videoListElement.children[2].children[0];
+     
+     const title = blockLink.children[1].children[0].children[1].innerHTML.trim();
+     const link = blockLink.href;
 
-		likedVideos.push({
-			'title': title,
-			'link': link
-		});
-	}
+     likedVideos.push({
+      'title': title,
+      'link': link
+     });
+    }
 
-	console.log(JSON.stringify(likedVideos));
+    console.log(JSON.stringify(likedVideos));
 }
 
 function getVideoListElements() {
-	return document.getElementById("contents").children[0].children[2].children[0].children[0].children;
+    return document.getElementsByTagName("ytd-playlist-video-renderer");
 }
