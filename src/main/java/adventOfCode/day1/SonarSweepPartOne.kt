7653806f37e2845b2ca25c057c2358ba41c6007b
Dicee <dcourtinot@gmail.com>
2022-01-19 01:21:55 +0100

    Add solutions to the first 2 days of Advent Of Code in Kotlin

---
+++ src/main/java/adventOfCode/day1/SonarSweepPartOne.kt
@@ -0,0 +1,23 @@
+package adventOfCode.day1
+
+// Difficulty: trivial
+
+// https://adventofcode.com/2021/day/1
+fun main() {
+    val depths = SonarSweep.depths
+    var counter = 0
+
+    for (i in 1 until depths.size - 1) {
+        if (depths[i - 1] < depths[i]) counter ++
+    }
+
+    println(counter)
+}
+
+object SonarSweep {
+    val depths = SonarSweep::class.java.getResource("depths.txt")!!
+            .readText()
+            .trim()
+            .split('\n')
+            .map { it.toInt() }
+}
---
+++ src/main/java/adventOfCode/day2/DivePartTwo.kt
@@ -0,0 +1,21 @@
+package adventOfCode.day2
+
+// Difficulty: trivial
+
+// https://adventofcode.com/2021/day/2#part2
+fun main() {
+    val finalState = Dive.instructions.asSequence()
+        .map { instruction ->
+            val (action, rawStep) = instruction.split(' ')
+            val step = rawStep.toInt()
+
+            when (action) {
+                "forward" -> { pos:Int, depth: Int, aim: Int -> Triple(pos + step, depth + step * aim, aim) }
+                "up" -> { pos: Int, depth: Int, aim: Int -> Triple(pos, depth, aim - step) }
+                else -> { pos: Int, depth: Int, aim: Int -> Triple(pos, depth, aim + step) }
+            }
+        }
+        .fold(Triple(0, 0, 0)) { acc, instruction -> instruction(acc.first, acc.second, acc.third) }
+
+    println(finalState.first * finalState.second)
+}
