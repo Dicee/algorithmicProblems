fc0035d04826fb67e459d91c6890fb7f27861c52
Dicee <Dicee@users.noreply.github.com>
2023-04-09 01:24:19 +0200

    Updated and simplified the way to get the video titles and links. Should be more stable/easier to fix with this method, even if Youtube changes its layout.

--- src/main/java/miscellaneous/tools/retrieve-youtube-liked-videos.js
+++ src/main/java/miscellaneous/tools/retrieve-youtube-liked-videos.js
@@ -1,32 +1,52 @@
 scrollUntilReachedBottom(printLikedVideos);
 
 function scrollUntilReachedBottom(onScrolledToBottom) {
+    const currentVideosCount = getVideoListElements().length;
     window.scrollBy(0, 10000);
 
-    const currentVideosCount = getVideoListElements().length;
     setTimeout(() => {
-     const newVideosCount = getVideoListElements().length;
-     if (newVideosCount > currentVideosCount) scrollUntilReachedBottom(onScrolledToBottom);
-     else onScrolledToBottom();
-    }, 2000)
+        const newVideosCount = getVideoListElements().length;
+        if (newVideosCount > currentVideosCount) scrollUntilReachedBottom(onScrolledToBottom);
+        else onScrolledToBottom();
+    }, 4000)
 }
 
 function printLikedVideos() {
     const likedVideos = [];
+    let count = 1;
 
-    for (const videoListElement of getVideoListElements()) {
-     const blockLink = videoListElement.children[1].children[0];
-     
-     const title = blockLink.children[1].children[0].children[1].innerHTML.trim();
-     const link = blockLink.href;
-
-     likedVideos.push({
-      'title': title,
-      'link': link
-     });
+    for (const link of getVideoLinkElements()) {
+        likedVideos.push({
+            'order': count,
+            'title': link.title,
+            'link': link.href
+        });
+        count++
     }
 
     console.log(JSON.stringify(likedVideos));
+}
+
+function printLikedVideos_old() {
+    const likedVideos = [];
+
+    for (const videoListElement of getVideoListElements()) {
+        const blockLink = videoListElement.children[1].children[0];
+
+        const title = blockLink.children[1].children[0].children[1].innerHTML.trim();
+        const link = blockLink.href;
+
+        likedVideos.push({
+            'title': title,
+            'link': link
+        });
+    }
+
+    console.log(JSON.stringify(likedVideos));
+}
+
+function getVideoLinkElements() {
+    return document.querySelectorAll("a#video-title");
 }
 
 function getVideoListElements() {
