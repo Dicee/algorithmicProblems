5beeb7abdb575867918369aee6ae72bc3a18f8f1
Dicee <Dicee@users.noreply.github.com>
2025-03-07 14:23:34 +0100

    Add solution to medium Leetcode problem

---
+++ src/main/java/leetCode/ReverseInteger.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int reverse(int x) {
+        int y = 0;
+        while (x != 0) {
+            int lastDigit = x % 10;
+            x /= 10;
+
+            if (y < 0 && y < (Integer.MIN_VALUE - lastDigit) / 10) {
+                return 0;
+            } else if (y > 0 && y > (Integer.MAX_VALUE - lastDigit) / 10) {
+                return 0;
+            }
+
+            y = 10 * y + lastDigit;
+        }
+        return y;
+    }
+}
